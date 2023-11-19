74a143a6cb8fe1d0f704d81e921541b2a7ce3fd7
Mark Moxon <mark@moxon.net>
2023-11-19 13:49:50 +0000

    Removed double-line empty comments and added missing Arguments header

--- 1-source-files/main-sources/elite-source.asm
+++ 1-source-files/main-sources/elite-source.asm
@@ -25783,6 +25783,8 @@ ENDIF
 \ This part draws the new sun. By the time we get to this point, the following
 \ variables should have been set up by parts 1 and 2:
 \
+\ Arguments:
+\
 \   V                   As we draw lines for the new sun, V contains the
 \                       vertical distance between the line we're drawing and the
 \                       centre of the new sun. As we draw lines and move up the
@@ -30502,7 +30504,6 @@ ENDIF
 \ We have killed a ship, so increase the kill tally, displaying an iconic
 \ message of encouragement if the kill total is a multiple of 256, and then
 \ make a nearby explosion sound.
-\
 \
 \ ******************************************************************************
 
