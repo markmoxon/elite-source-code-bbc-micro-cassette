6125a4de6eaa9a573922b718724e79510500d5cb
Mark Moxon <mark@moxon.net>
2023-12-16 14:35:09 +0000

    Added correct SKIP sizes to XX3 and K% workspaces

--- 1-source-files/main-sources/elite-source.asm
+++ 1-source-files/main-sources/elite-source.asm
@@ -867,7 +867,7 @@
 
 .XX3
 
- SKIP 0                 \ Temporary storage, typically used for storing tables
+ SKIP 256               \ Temporary storage, typically used for storing tables
                         \ of values such as screen coordinates or ship data
 
 \ ******************************************************************************
@@ -2603,7 +2603,7 @@ ENDMACRO
 
 .K%
 
- SKIP 0                 \ Ship data blocks and ship line heap
+ SKIP NOSH * NI%        \ Ship data blocks and ship line heap
 
 \ ******************************************************************************
 \
