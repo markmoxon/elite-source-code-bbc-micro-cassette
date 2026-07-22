e92b566814b0487668ad08fab7ae5c1541bb7dbd
Mark Moxon <mark@moxon.net>
2026-07-22 10:21:39 +0100

    Fix incorrect Delta 14B comment

--- 1-source-files/main-sources/elite-source.asm
+++ 1-source-files/main-sources/elite-source.asm
@@ -4084,7 +4084,7 @@ ENDMACRO
                         \
                         \   * &FF = Delta 14B
                         \
-                        \ Toggled by pressing "D" when paused, see the DK4
+                        \ Toggled by pressing "L" when paused, see the DK4
                         \ routine for details
 
                         \ --- End of added code ------------------------------->
